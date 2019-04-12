e2a4f490ad82b5889b55aeea983d817a6b31dd7f
patterncard <patcad@o2.pl>
2019-04-12 18:05:14 +0200

    #r3k72 Function for fetching

--- MyWeather/MyWeather.cpp
+++ MyWeather/MyWeather.cpp
@@ -8,7 +8,7 @@ int main()
 	CURL *curl = curl_easy_init();
 	if (curl) {
 		CURLcode res;
-		curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+		curl_easy_setopt(curl, CURLOPT_URL, "http://api.openweathermap.org/data/2.5/weather?q=Gliwice&appid=af84822059c4495720af00937a834639");
 		res = curl_easy_perform(curl);
 		curl_easy_cleanup(curl);
 	}
